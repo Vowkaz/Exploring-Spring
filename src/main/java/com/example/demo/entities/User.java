4ca5a0ac8a47f84430c9e6ca3bfbeaa4f421bac2
vowkaz <jlucasgarmier@gmail.com>
2023-03-11 17:12:39 -0400

    H2 database, test profile, JPA

--- src/main/java/com/example/demo/entities/User.java
+++ src/main/java/com/example/demo/entities/User.java
@@ -1,14 +1,19 @@
 package com.example.demo.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serial;
 import java.io.Serializable;
 
+@Entity
+@Table(name = "tb-user")
 public class User implements Serializable
 {
 
     @Serial
     private static final long serialVersionUID = 1L;
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String name;
     private String email;
